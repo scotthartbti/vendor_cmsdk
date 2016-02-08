f497e49e1503017ba92d922b8d541dad6cd53fb3
LorDClockaN <davor@losinj.com>
2016-02-08 15:06:53 +0100

    CMSDK: App picker tile (2/2)
    Change-Id: If340ee824e629ba3696e2c15e135904d82bd262a
    Signed-off-by: scott <scott@ubuntu>
    
    Conflicts:
    	src/java/org/cyanogenmod/internal/util/QSConstants.java

--- src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -65,6 +65,7 @@ public class QSConstants {
     public static final String TILE_NAVBAR = "navbar";	
     public static final String TILE_PIE = "pie";	
     public static final String TILE_APPCIRCLEBAR = "appcirclebar";
+    public static final String TILE_APP_PICKER = "app_picker";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
     public static final String DYNAMIC_TILE_IME_SELECTOR = "ime_selector";
@@ -120,6 +121,7 @@ public class QSConstants {
 	STATIC_TILES_AVAILABLE.add(TILE_NAVBAR);
 	STATIC_TILES_AVAILABLE.add(TILE_PIE);
 	STATIC_TILES_AVAILABLE.add(TILE_APPCIRCLEBAR);
+	STATIC_TILES_AVAILABLE.add(TILE_APP_PICKER);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
 
