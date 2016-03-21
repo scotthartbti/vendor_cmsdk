e699b39f4a5a62f6898447e18a012ac4f6670d41
scott <scott@ubuntu>
2016-03-21 19:34:34 -0400

    Revert "Revert "CMSDK: Themes Tile (2/2)""
    This reverts commit c80f8d29c456b8424c4667f566e5e643e08d96ae.

--- src/java/org/cyanogenmod/internal/util/QSConstants.java
+++ src/java/org/cyanogenmod/internal/util/QSConstants.java
@@ -65,6 +65,7 @@ public class QSConstants {
     public static final String TILE_NAVBAR = "navbar";	
     public static final String TILE_APPCIRCLEBAR = "appcirclebar";
     public static final String TILE_APP_PICKER = "app_picker";
+    public static final String TILE_THEMES = "themes";
     public static final String TILE_SOUND = "sound";
 
     public static final String DYNAMIC_TILE_NEXT_ALARM = "next_alarm";
@@ -121,6 +122,7 @@ public class QSConstants {
 	STATIC_TILES_AVAILABLE.add(TILE_NAVBAR);
 	STATIC_TILES_AVAILABLE.add(TILE_APPCIRCLEBAR);
 	STATIC_TILES_AVAILABLE.add(TILE_APP_PICKER);
+        STATIC_TILES_AVAILABLE.add(TILE_THEMES);
 	STATIC_TILES_AVAILABLE.add(TILE_SOUND);
 
         TILES_AVAILABLE.addAll(STATIC_TILES_AVAILABLE);
